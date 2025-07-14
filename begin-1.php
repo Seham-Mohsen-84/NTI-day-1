b9796d3631b8013945c0df80c89f1f7f1ecb7e5c
seham mohsen <sehammohsen84@gmail.com>
2025-07-14 09:19:46 +0300

    first commit

---
+++ begin-1.php
@@ -0,0 +1,41 @@
+<html>
+   <form action="" method="post">
+       <input type="text" name="UserName"><br>
+       <input type="submit" name="Sent"><br>
+   </form>
+</html>
+
+<?php
+
+echo $_POST["UserName"];
+define("seham","mohsen");
+echo "<br>";
+echo seham;
+/*print "my double quits \\\"";
+echo "<br>";
+echo "<br>";
+
+print nl2br("welcome to my 
+chanel I hope
+you will find your self here ");
+
+echo "<br>";
+
+
+echo <<<"here"
+Hello php 
+we are done here special chars
+so there are ""\\$$
+here;
+
+echo "<br>";
+
+echo <<<'Now'
+Hello php 
+we are done here special chars
+so there are ""\\$$
+Now;
+
+echo "<br>";
+
+print_r(["egy"=>"egypt","euo"=>"europa"]);*/
